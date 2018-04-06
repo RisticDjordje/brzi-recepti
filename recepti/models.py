eeb211c9ba512e9a999f6d9beac73c0b9fc72a66
Djordje Ristic <risticdj81@gmail.com>
2018-04-06 19:28:46 +0200

    recepti

--- recepti/models.py
+++ recepti/models.py
@@ -1,21 +1,21 @@
 from django.db import models
 
 
-class Sastojci(models.Model):                                                 # npr mrkva, kupus, krompir, banana...
-    ime = models.CharField(max_length=50)
-
-    class Meta:
-        verbose_name_plural = "Sastojci"
-
-    def __str__(self):
-        return self.ime
-
-
 class Kategorije(models.Model):                                               # dorucak, rucak, vecera, dezert
     ime = models.CharField(max_length=100)
 
     class Meta:
         verbose_name_plural = "Kategorije"
+
+    def __str__(self):
+        return self.ime
+
+
+class Sastojci(models.Model):                                                 # npr mrkva, kupus, krompir, banana...
+    ime = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name_plural = "Sastojci"
 
     def __str__(self):
         return self.ime
@@ -35,4 +35,3 @@ class Jela(models.Model):
 
     def __str__(self):
         return self.ime
-
